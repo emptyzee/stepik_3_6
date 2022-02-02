7428c9ab62e205d49c27689b8ff6876f76f0dc4c
Aleksandr <92896866+emptyzee@users.noreply.github.com>
2022-02-02 22:22:25 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -7,6 +7,5 @@ import time
 def test_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(30)
-    basket = browser.find_element_by_class_name('btn btn-lg btn-primary btn-add-to-basket')
-    
+    time.sleep(10)
+    basket = browser.find_element_by_css_selector('#add_to_basket_form>button[type="submit"]')
